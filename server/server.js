0e10a67949dc81ed238facb3780354c8f213bd82
kshao5 <465217430@163.com>
2017-09-02 00:06:31 -0500

    Init commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// root of our application, create express app, configure public directory, app.listen to start the server
+
+const path = require('path');
+// load express
+const express = require('express');
+// address passed to the express middleware
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(publicPath));
+app.listen(3000, function(){
+	console.log("server is up on ${port}");
+})
+// displays current directory
+//console.log(__dirname + "/../public");
+//console.log(publicPath);
